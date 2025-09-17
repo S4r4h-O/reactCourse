bd2a7fafe32bb6fcb24af83c62eb07cb2e7502f7
Sarah <sarah.shofani@proton.me>
2025-09-17 18:35:29 -0300

    Controlling which data should be deferred

--- spaApp2/frontend/src/pages/EventDetailPage.jsx
+++ spaApp2/frontend/src/pages/EventDetailPage.jsx
@@ -1,33 +1,63 @@
+import { Suspense } from "react";
 import EventItem from "../components/EventItem";
-import { redirect, useRouteLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
+import { Await, redirect, useRouteLoaderData } from "react-router-dom";
 
 export default function EventDetailItem() {
-  const data = useRouteLoaderData("event-detail");
-  const event = data.event;
+  const { event, events } = useRouteLoaderData("event-detail");
 
   return (
-    <div>
-      <EventItem event={event} />
-    </div>
+    <>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
   );
 }
 
 export async function loader({ request, params }) {
   const id = params.eventId;
+
+  return {
+    event: await loadEvent(id),
+    events: loadEvents(),
+  };
+}
+
+async function loadEvent(id) {
   const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
     throw new Response(
-      JSON.stringify({
-        message: "Could not fetch details for selected event.",
-      }),
-      {
-        status: 500,
-        headers: { "Content-Type": "application/json" },
-      },
+      { message: "Could not fetch details for selected event." },
+      { status: 500 },
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
+  }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  } else {
+    const resData = await response.json();
+    return resData.events;
   }
 }
 

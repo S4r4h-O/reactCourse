87bf4afd8bf5af6daec4e1d5a86f76358e9779b0
Sarah <sarah.shofani@proton.me>
2025-09-18 11:59:11 -0300

    User login, Authorization and Attaching Auth Tokens to Outgoing Requests

--- authentication/frontend/src/pages/Authentication.js
+++ authentication/frontend/src/pages/Authentication.js
@@ -38,5 +38,10 @@ export async function action({ request }) {
     );
   }
 
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+
   return redirect("/");
 }

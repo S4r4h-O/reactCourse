26bd7c29220eeaa5a45f7653565bd6125c083b55
Sarah <sarah.shofani@proton.me>
2025-09-18 18:29:23 -0300

    Diving Into Generics

--- reactTS/basics.ts
+++ reactTS/basics.ts
@@ -54,3 +54,15 @@ function add(a: number, b: number) {
 function printOpt(value: any) {
   console.log(value);
 } // Void type
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+
+const stringArray = insertAtBeginning(["a", "b", "c"], "d");

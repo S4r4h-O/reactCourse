9dfa2f1ef0242d2f9bcb433fd34a96d578792cde
Sarah <sarah.shofani@proton.me>
2025-09-22 11:44:08 -0300

    A Different Way Of Consuming Context (was not used)

--- advancedStateManagement/src/components/Cart.jsx
+++ advancedStateManagement/src/components/Cart.jsx
@@ -12,36 +12,38 @@ export default function Cart({ onUpdateItemQuantity }) {
   const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
 
   return (
-    <div id="cart">
-      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
-      {cartCtx.items.length > 0 && (
-        <ul id="cart-items">
-          {cartCtx.items.map((item) => {
-            const formattedPrice = `$${item.price.toFixed(2)}`;
+    <CartContext>
+      <div id="cart">
+        {cartCtx.items.length === 0 && <p>No items in cart!</p>}
+        {cartCtx.items.length > 0 && (
+          <ul id="cart-items">
+            {cartCtx.items.map((item) => {
+              const formattedPrice = `$${item.price.toFixed(2)}`;
 
-            return (
-              <li key={item.id}>
-                <div>
-                  <span>{item.name}</span>
-                  <span> ({formattedPrice})</span>
-                </div>
-                <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
-                    -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
-                    +
-                  </button>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-      <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p>
-    </div>
+              return (
+                <li key={item.id}>
+                  <div>
+                    <span>{item.name}</span>
+                    <span> ({formattedPrice})</span>
+                  </div>
+                  <div className="cart-item-actions">
+                    <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                      -
+                    </button>
+                    <span>{item.quantity}</span>
+                    <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                      +
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        <p id="cart-total-price">
+          Cart Total: <strong>{formattedTotalPrice}</strong>
+        </p>
+      </div>
+    </CartContext>
   );
 }

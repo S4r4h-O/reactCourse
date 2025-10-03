8b49ae025da6fb04cbee337f891dcd80100f1618
Sarah <sarah@F680V9.0>
2025-10-03 11:07:26 -0300

    Outputting Meals Data & Images With Unknown Dimensions

--- foodies-starting-project/app/meals/page.js
+++ foodies-starting-project/app/meals/page.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
+import cssClasses from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals Page</h1>
-    </main>
+    <>
+      <header className={cssClasses.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={cssClasses.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun.
+        </p>
+        <p className={cssClasses.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={cssClasses.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }

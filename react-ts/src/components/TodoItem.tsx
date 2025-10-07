2d035308520ae75c99c9da8a72411a004a3c75f5
Sarah <sarah@F680V9.0>
2025-10-07 14:17:40 -0300

    Time to Practice Removing a Todo

--- react-ts/src/components/TodoItem.tsx
+++ react-ts/src/components/TodoItem.tsx
@@ -1,6 +1,15 @@
 import Todo from "../models/todo";
 import cssClasses from "./TodoItem.module.css";
 
-export default function TodoItem({ item }: { item: Todo }) {
-  return <li className={cssClasses.item}>{item.text}</li>;
+type Props = {
+  item: Todo;
+  onRemove: () => void;
+};
+
+export default function TodoItem({ item, onRemove }: Props) {
+  return (
+    <li className={cssClasses.item} onClick={onRemove}>
+      {item.text}
+    </li>
+  );
 }
--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -2,11 +2,20 @@ import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 import cssClasses from "./Todos.module.css";
 
-function Todos({ items }: { items: Todo[] }) {
+type Props = {
+  items: Todo[];
+  onRemoveTodo: (id: string) => void;
+};
+
+function Todos({ items, onRemoveTodo }: Props) {
   return (
     <ul className={cssClasses.todos}>
       {items.map((item) => (
-        <TodoItem key={item.id} item={item} />
+        <TodoItem
+          key={item.id}
+          item={item}
+          onRemove={onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );

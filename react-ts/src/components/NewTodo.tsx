8bd6c15de9e7f56e2f59e3cb8c0e463872a07e55
Sarah <sarah@F680V9.0>
2025-10-07 12:37:16 -0300

    Working with Function Props

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,18 +1,24 @@
 import { useRef } from "react";
 
-export default function NewTodo() {
+export default function NewTodo({
+  onAddTodo,
+}: {
+  onAddTodo: (text: string) => void;
+}) {
   const inputRef = useRef<HTMLInputElement>(null);
 
   function submitHandler(e: React.FormEvent) {
     e.preventDefault();
 
     const todoText = inputRef.current!.value;
+
+    onAddTodo(todoText);
   }
 
   return (
     <form onSubmit={submitHandler}>
-      <label htmlFor="">Todo Text</label>
-      <input ref={inputRef} type="text" />
+      <label htmlFor="text">Todo Text</label>
+      <input ref={inputRef} id="text" type="text" />
       <button>Add Todo</button>
     </form>
   );

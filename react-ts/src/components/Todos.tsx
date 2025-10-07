7222d70b93aa73547df7dbba2eae67d03682c7ba
Sarah <sarah@F680V9.0>
2025-10-07 11:58:28 -0300

    Time to Practice Exercise Time!

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+function Todos({ items }: { items: Todo[] }) {
   return (
     <ul>
-      {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+      {items.map((item) => (
+        <TodoItem key={item.id} item={item} />
       ))}
     </ul>
   );
-};
+}
 
 export default Todos;
